04fecd3d04de5d9b55f2ba4476dda75be66feceb
Francisco de Souza Junior <chico@chico.net.br>
2017-08-27 12:35:40 -0300

    Licensing comments

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dancing-groot-esp8266
-ESP8266 code (made in Arduino IDE) from Dancing Groot Project
+ESP8266 code (made in Arduino IDE) from Dancing Groot(tm) Project
+
+GROOT is trademark from Marvel Characters, Inc. and I use it here only for educational and hobbyist purposes, without any financial reward. 
--- dancing-groot-esp8266.ino
+++ dancing-groot-esp8266.ino
@@ -1,3 +1,29 @@
+/*
+ *  This file is part of Dancing Groot Project
+ *  Copyright (c) 2017 Francisco de Souza Junior (chico.net.br).
+ *  
+ *  GROOT is trademark from Marvel Characters, Inc. and I use it here 
+ *  only for educational and hobbyist purposes, without any financial 
+ *  reward.
+ *
+ *  Foobar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ */
+
+#include <Servo.h>
+
 void setup() {
   // put your setup code here, to run once:
 
